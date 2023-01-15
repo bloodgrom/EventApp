ff3bed7d5a5e5dfc69d068820a5fb1cc40a18d5d
Anton Minkov <a.minkov117@gmail.com>
2023-01-15 03:57:19 +0000

    Restoration scripts

---
+++ create_db_backup.py
@@ -0,0 +1,18 @@
+import shutil
+import os
+from datetime import date
+
+today = date.today()
+#today = today.strftime("%d-%m-%Y")
+
+script_path = os.path.abspath(__file__) # i.e. /path/to/dir/foobar.py
+script_dir = os.path.split(script_path)[0] #i.e. /path/to/dir/
+
+rel_path_origin = "CalendarApp\db.sqlite3"
+rel_path_backup = "CalendarApp\db_backup\db_" + str(today) + ".sqlite3"
+original_db = os.path.join(script_dir, rel_path_origin)
+backup_db = os.path.join(script_dir, rel_path_backup)
+
+shutil.copyfile(original_db, backup_db)
+
+print("DB copied successfuly")
